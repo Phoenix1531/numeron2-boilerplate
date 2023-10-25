9e93156ddb147ef1f8ba75dd14a1c8dfe226627b
phoenix1531 <arjun15310@gmail.com>
2023-10-25 21:53:06 +0530

    Numeron 2 done

--- score.js
+++ score.js
@@ -1,1 +1,13 @@
 // Iteration 8: Making scoreboard functional
+
+let params=new URLSearchParams(window.location.search)
+
+let score = params.get("score")
+
+let scoreBox=document.getElementById("score-board")
+scoreBox.textContent=score
+
+let PlayagainBtn=document.getElementById("play-again-button")
+PlayagainBtn.onclick=()=>{
+    window.location.href="./game.html"
+}
